7df0c3b6329463621dc96240b3da7fc3ce3ae6c9
Paul VanKeuren <paul.vankeuren@gmail.com>
2017-01-19 21:53:54 -0600

    added npc animation sheets and generated jsons

---
+++ bin/animgen.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+import 'dart:convert';
+
+
+main() async {
+  throw('dont use');
+  Directory npcDirectory = new Directory('web/images/npc');
+
+  List<Directory> npcs = npcDirectory.listSync();
+  for (Directory fse in npcs) {
+    String folderName = fse.path.split('/').last;
+    String newFilePath = fse.path + '/' + folderName + '.json';
+    List<File> anims = fse.listSync();
+
+    List animation = [];
+
+    for (File anim in anims) {
+      String animName = anim.path.split('/').last
+        .replaceAll(folderName + '_', '')
+        .replaceAll('.png', '');
+
+        animation.add({
+          'image': 'images/npc/' + folderName + '/' + anim.path.split('/').last,
+          'height': 0,
+          'width': 0,
+          'animations': {
+            animName: {
+              'frames': []
+            }
+          }
+        });
+    }
+    JsonEncoder encoder = new JsonEncoder.withIndent('  ');
+    String result = encoder.convert(animation);
+    File animFile = new File(newFilePath);
+    await animFile.create();
+    animFile.writeAsString(result);
+  }
+
+}
---
+++ bin/rename_npc.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+
+main() async {
+    throw('dont use');
+  Directory npcDirectory = new Directory('web/images/npc');
+
+  npcDirectory.list(recursive: true).listen((FileSystemEntity fse) {
+    if (fse is File && fse.path.endsWith('.png')) {
+      String oldName = fse.path.split('/').last;
+      List segments = oldName.split('_');
+
+      int before = segments.indexOf('x1') + 1;
+      int after = segments.indexOf('png');
+      if (before == -1 || after == -1) {
+        throw(oldName);
+      }
+      String action = segments.sublist(before, after).join('_');
+
+      String newName = fse.parent.path.split('/').last + '_' + action + '.png';
+
+      fse.rename(fse.parent.path + '/' + newName);
+    }
+  });
+
+
+
+}
