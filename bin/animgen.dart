93932510a9f1aac7769b9759c3114f4ac7aa4356
Paul VanKeuren <paul.vankeuren@gmail.com>
2017-03-09 11:12:33 -0600

    animal json's are complete. Need to rewrite Animation class to use the new format.

--- bin/animgen.dart
+++ bin/animgen.dart
@@ -8,29 +8,29 @@ main() async {
   for (Directory fse in npcs) {
     String folderName = fse.path.split('/').last;
     String newFilePath = fse.path + '/' + folderName + '.json';
-    List<File> anims = fse.listSync();
+    if (new File(newFilePath).existsSync() == false) {      
+      List<File> anims = fse.listSync();
+      List animation = [];
+      for (File anim in anims.where((file) => file.path.endsWith('.png'))) {
+        String animName = anim.path.split('/').last
+          .replaceAll(folderName + '_', '')
+          .replaceAll('.png', '');
 
-    List animation = [];
-
-    for (File anim in anims.where((file) => file.path.endsWith('.png'))) {
-      String animName = anim.path.split('/').last
-        .replaceAll(folderName + '_', '')
-        .replaceAll('.png', '');
-
-        animation.add(
-          {
-            'image': 'packages/ur/assets/animal/' + folderName + '/' + anim.path.split('/').last,
-            'name': animName,
-            'frames': 1,
-            'loops': true
-          }
-      );
+          animation.add(
+            {
+              'image': 'packages/ur/assets/animal/' + folderName + '/' + anim.path.split('/').last,
+              'name': animName,
+              'frames': 1,
+              'loops': true
+            }
+        );
+      }
+      JsonEncoder encoder = new JsonEncoder.withIndent('  ');
+      String result = encoder.convert(animation);
+      File animFile = new File(newFilePath);
+      await animFile.create();
+      animFile.writeAsString(result);
     }
-    JsonEncoder encoder = new JsonEncoder.withIndent('  ');
-    String result = encoder.convert(animation);
-    File animFile = new File(newFilePath);
-    await animFile.create();
-    animFile.writeAsString(result);
   }
 
 }
