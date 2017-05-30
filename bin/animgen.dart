873af3ff461b889a17557d5eb6a641f6ce33d544
Paul VanKeuren <paul.vankeuren@gmail.com>
2017-05-29 19:37:20 -0500

    counting trant frames

--- bin/animgen.dart
+++ bin/animgen.dart
@@ -2,28 +2,34 @@ import 'dart:io';
 import 'dart:convert';
 
 main() async {
-  Directory npcDirectory = new Directory('lib/assets/animal');
+  Directory npcDirectory = new Directory('lib/assets/trant');
 
   List<Directory> npcs = npcDirectory.listSync();
   for (Directory fse in npcs) {
-    String folderName = fse.path.split('/').last;
-    String newFilePath = fse.path + '/' + folderName + '.json';
-    if (new File(newFilePath).existsSync() == false) {      
+    if (fse.path.contains('paper_tree')) {
+      String folderName = fse.path.split('/').last;
+      String newFilePath = fse.path + '/' + folderName + '.json';
+      if (new File(newFilePath).existsSync() == false) {
+        new File(newFilePath).deleteSync();
+      }
+
       List<File> anims = fse.listSync();
       List animation = [];
       for (File anim in anims.where((file) => file.path.endsWith('.png'))) {
-        String animName = anim.path.split('/').last
-          .replaceAll(folderName + '_', '')
-          .replaceAll('.png', '');
+        String animName = anim.path
+            .split('/')
+            .last
+            .replaceAll(folderName + '_', '')
+            .replaceAll('.png', '');
 
-          animation.add(
-            {
-              'image': 'packages/ur/assets/animal/' + folderName + '/' + anim.path.split('/').last,
-              'name': animName,
-              'frames': 1,
-              'loops': true
-            }
-        );
+        animation.add({
+          'image': 'packages/ur/assets/trant/' +
+              folderName +
+              '/' +
+              anim.path.split('/').last,
+          'name': animName,
+          'frames': 1
+        });
       }
       JsonEncoder encoder = new JsonEncoder.withIndent('  ');
       String result = encoder.convert(animation);
@@ -32,5 +38,4 @@ main() async {
       animFile.writeAsString(result);
     }
   }
-
 }
--- bin/jsontweaks.dart
+++ bin/jsontweaks.dart
@@ -2,20 +2,23 @@ import 'dart:io';
 import 'dart:convert';
 
 main() async {
-  String animal = 'firefly';
-  int frameWidth = 87;
-  int frameHeight = 40;
+  String animal = 'trant_bubble';
+  int frameHeight = 4;
+  int frameWidth = 19;
+  String name = '10';
 
-  File json = new File('lib/assets/animal/$animal/$animal.json');
+  File json = new File('lib/assets/trant/$animal/$animal.json');
   if (json.existsSync()) {
     String jsontext = await json.readAsString();
     List animations = JSON.decode(jsontext);
 
     for (Map anim in animations) {
-      anim['frame'] = {
-        'width': frameWidth,
-        'height': frameHeight
-      };
+      if (anim['name'] == name) {
+        anim['frame'] = {
+          'width': frameWidth,
+          'height': frameHeight
+        };
+      }
     }
 
     animations.sort((Map a, Map b) => a['name'].compareTo(b['name']) );
